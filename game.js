42d36c5fbd08fbb50c85806c855cd8e7bc1ad8d2
Kevin Black <Kevin_Jay_Black@hotmail.com>
2016-10-25 14:23:58 -0600

    feat(core) Add requestAnimationFrame
    Browser API call that refreshes the screen 60 times per second

--- game.js
+++ game.js
@@ -4,11 +4,13 @@
     var screen = canvas.getContext('2d');
     var gameSize = { x: canvas.width, y: canvas.height };
 
+    this.bodies = [new Player];
+
     var self = this;
     var tick = function () {
       self.update();
-      self.draw(sreen, gameSize);
-      requestAnimationFrame
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);//browswer API call to refresh screen
     };
 
     tick();
@@ -16,8 +18,12 @@
 
   Game.prototype = {
     update: function() {
-
+      // console.log("good day");
     },
+
+    draw: function() {
+      screen.fillRect(30, 30, 40, 40);
+    }
   };
 
   window.onload = function() {
